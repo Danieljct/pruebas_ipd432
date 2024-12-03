a6e24ee111505151aa9099412ed72e692fc1553d
Daniel Josué <daniel.cubillost@usm.cl>
2024-12-03 15:16:26 -0300

    cambio en clk
    170MHz

--- tarea3_srcs/TOP_module.sv
+++ tarea3_srcs/TOP_module.sv
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module TOP_module #(parameter baudrate = 115200, parameter clk_base = 130000000, parameter N = 4)(
+module TOP_module #(parameter baudrate = 115200, parameter clk_base = 170000000, parameter N = 10)(
 	input  logic               clk_100M,
 	input  logic               reset_n,
 	input  logic               uart_rx,
