d1c7a6de35e63697f3542722a9da24ac5d4a3938
Daniel Josué <daniel.cubillost@usm.cl>
2024-12-03 16:09:55 -0300

    cambios matlab

--- tarea3_srcs/MATLAB/coprocessorTesting.m
+++ tarea3_srcs/MATLAB/coprocessorTesting.m
@@ -7,9 +7,10 @@ N = 1024;  % define el numero de elementos de cada vector
 A=uint8(ceil(rand(N,1)*254));
 B=uint8(ceil(rand(N,1)*254));
 
-%B = uint8(zeros(1024,1));
+%B = uint8(zeros(N,1));
 %A = B + 255;
-%B(1024) = A(1024);5
+%B = ((1:N)-1)';
+%B(1024) = A(1024);
 %Guarda vectores A y B (cada uno de una columna de 1024 filas) en un
 %archivo de texto. Cada linea del archivo contiene un elemento.
 h= fopen('VectorA.txt', 'w');
@@ -20,11 +21,12 @@ h= fopen('VectorB.txt', 'w');
 fprintf(h, '%i\n', B);
 fclose(h);
 
+
 % Calcula valores de referencia para las operaciones, realizadas en forma local en el host
 sumVec_host = uint8(double(A)+double(B)-(double(A)+double(B)>255)*256);
 avgVec_host = uint8((floor(double(A)/2)+floor(double(B)/2)));
 man_host = sum(abs(double(B)-double(A)));
-euc_host =  sqrt(sum((double(A)-double(B)).^2));
+%euc_host =  sqrt(sum((double(A)-double(B)).^2));
 
 %% A partir de aca se realizan las operaciones por medio de comandos al coprocesador
 
@@ -48,7 +50,7 @@ VecB_device = uint8(command2dev('readVec', 'BRAMB', COM_port));
 sumVec_device = uint8(command2dev('sumVec', COM_port)); %realiza la suma elemento a elemento de los vectores almacenados y envia el resultado por la UART
 avgVec_device = uint8(command2dev('avgVec', COM_port));
 man_device = command2dev('manDist', COM_port); %realiza el calculo de la distancia de Manhattan entre dos vectores y envia el resultado por la UART
-euc_device = command2dev('eucDist', COM_port); %realiza el calculo de la distancia Euclideana entre dos vectores y envia el resultado por la UART
+%euc_device = command2dev('eucDist', COM_port); %realiza el calculo de la distancia Euclideana entre dos vectores y envia el resultado por la UART
 
 %% Validacion.
 % Los resultados _diff deberian ser 0 (o cercanos, dependiendo de su
@@ -59,4 +61,4 @@ diffB = sum(abs(double(VecB_device') - double(B)))
 sumVec_diff = sum(double(sumVec_host') - double(sumVec_device))
 avgVec_diff = sum(double(avgVec_host') - double(avgVec_device))
 man_diff = double(man_host) - double(man_device)
-euc_diff = euc_host - euc_device
+%euc_diff = euc_host - euc_device
